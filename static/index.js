e4609c28d10fa4ef73ad8896caba466323226fc0
huijie123 <98313070+huijie123@users.noreply.github.com>
2022-01-24 19:24:54 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return '垃圾砍你牛子';
+    if (score <= 99) return '什么撸，啊音游玩家';
+    if (score <= 149) return '我超，您';
+    if (score <= 199) return '给你嗦牛子';
     return '人？';
 }
 
